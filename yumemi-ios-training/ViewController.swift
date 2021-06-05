88c26392301dc373e81b4ea21af880bcff67be28
Moto <mott.eng0124@gmail.com>
2021-06-05 21:14:26 +0900

    Initial Commit

---
+++ yumemi-ios-training/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  yumemi-ios-training
+//
+//  Created by Motoyuki Ito on 6/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
